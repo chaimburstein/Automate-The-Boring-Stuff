a2bde67c2691d6588916bfc21cb3e3a38a332eb5
chaimburstein <66576265+chaimburstein@users.noreply.github.com>
2021-08-14 16:14:00 -0400

    Add files via upload

---
+++ guessTheNumber.py
@@ -0,0 +1,24 @@
+#Guess the number game
+
+import random
+
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20')
+
+#ask the player to guess six times
+for guessesTaken in range(1, 7, 1):
+    print('Take a guess')
+    guess=int(input())
+    if guess<secretNumber:
+        print('Your guess is too low')
+    elif guess>secretNumber:
+        print('Your guess is too high')
+    else:
+        break
+
+if guess == secretNumber:
+    print('You guessed the correct number in ' + str(guessesTaken) +' guesses')
+else:
+    print('Nope! The secret number I was thinking of was' + str(secretNumber))
+
+
